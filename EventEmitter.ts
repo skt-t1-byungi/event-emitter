5688872692b5174937dbda515e6accee0535811e
skt-t1-byungi <tiniwz@gmail.com>
2019-02-11 16:45:25 +0900

    reformat

--- EventEmitter.ts
+++ EventEmitter.ts
@@ -7,69 +7,69 @@ type OnceListener = Listener & {key: Listener}
 interface ListenerMap { [name: string]: Array<Listener | OnceListener > }
 
 class EventEmitter {
-  private _listeners: ListenerMap = {}
+    private _listeners: ListenerMap = {}
 
-  public on (name: string, listener: Listener) {
-    assertListener(listener)
+    public on (name: string, listener: Listener) {
+        assertListener(listener)
 
-    if (!this.has(name)) this._listeners[name] = []
-    this._listeners[name].push(listener)
-  }
-
-  public off (name: string, listener?: Listener) {
-    if (listener) assertListener(listener)
-
-    if (!this.has(name)) return
-
-    if (!listener) {
-      delete this._listeners[name]
-      return
+        if (!this.has(name)) this._listeners[name] = []
+        this._listeners[name].push(listener)
     }
 
-    this._listeners[name] = filter(this._listeners[name], target => !equalListener(target, listener))
+    public off (name: string, listener?: Listener) {
+        if (listener) assertListener(listener)
 
-    if (this._listeners[name].length === 0) delete this._listeners[name]
-  }
+        if (!this.has(name)) return
 
-  public once (name: string, listener: Listener) {
-    assertListener(listener)
+        if (!listener) {
+            delete this._listeners[name]
+            return
+        }
 
-    const onceListener = ((...params: any[]) => {
-      this.off(name, listener)
-      listener(...params)
-    }) as OnceListener
+        this._listeners[name] = filter(this._listeners[name], target => !equalListener(target, listener))
 
-    onceListener.key = listener
+        if (this._listeners[name].length === 0) delete this._listeners[name]
+    }
 
-    this.on(name, onceListener)
-  }
+    public once (name: string, listener: Listener) {
+        assertListener(listener)
 
-  public emit (name: string, ...params: any[]) {
-    if (!this.has(name)) return
-    forEach(this._listeners[name].slice(), listener => listener(...params))
-  }
+        const onceListener = ((...params: any[]) => {
+            this.off(name, listener)
+            listener(...params)
+        }) as OnceListener
 
-  public has (name: string, listener?: Listener) {
-    if (!hasOwn(this._listeners, name)) return false
-    if (!listener) return true
+        onceListener.key = listener
 
-    assertListener(listener)
+        this.on(name, onceListener)
+    }
 
-    return findIndex(this._listeners[name], target => equalListener(target, listener)) > -1
-  }
+    public emit (name: string, ...params: any[]) {
+        if (!this.has(name)) return
+        forEach(this._listeners[name].slice(), listener => listener(...params))
+    }
+
+    public has (name: string, listener?: Listener) {
+        if (!hasOwn(this._listeners, name)) return false
+        if (!listener) return true
+
+        assertListener(listener)
+
+        return findIndex(this._listeners[name], target => equalListener(target, listener)) > -1
+    }
 }
 
 export = EventEmitter
 
 function assertListener (listener: Listener) {
-  const type = typeof listener
-  if (type !== 'function') throw new TypeError(`Expected listener to be a function, but ${type}`)
+    const type = typeof listener
+    if (type !== 'function') throw new TypeError(`Expected listener to be a function, but ${type}`)
 }
 
 function hasOwn (obj: object, key: string) {
-  return Object.prototype.hasOwnProperty.call(obj, key)
+    return Object.prototype.hasOwnProperty.call(obj, key)
 }
 
 function equalListener (target: Listener | OnceListener, listener: Listener) {
-  return target === listener || (target as OnceListener).key === listener
+    return target === listener || (target as OnceListener).key === listener
 }
